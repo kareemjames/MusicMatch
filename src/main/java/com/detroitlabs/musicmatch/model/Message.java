ae1ab0c0655e742dd6a8794385b993361b2bad60
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-03-11 14:24:47 -0400

    Create Body class.

--- src/main/java/com/detroitlabs/musicmatch/model/Message.java
+++ src/main/java/com/detroitlabs/musicmatch/model/Message.java
@@ -1,4 +1,21 @@
 package com.detroitlabs.musicmatch.model;
 
 public class Message {
+
+    private Body body;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "body=" + body +
+                '}';
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
 }
