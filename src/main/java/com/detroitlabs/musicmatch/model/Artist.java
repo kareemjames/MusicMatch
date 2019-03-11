6c61d4defeb27287bb17d5bed2b30ab07feacab9
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-03-11 14:34:24 -0400

    Fill in properties for Artist class.

--- src/main/java/com/detroitlabs/musicmatch/model/Artist.java
+++ src/main/java/com/detroitlabs/musicmatch/model/Artist.java
@@ -1,7 +1,55 @@
 package com.detroitlabs.musicmatch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Artist {
 
+    private int artist_id;
+    private String artist_name;
+    private String artist_country;
+    private int artist_rating;
 
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artist_id=" + artist_id +
+                ", artist_name='" + artist_name + '\'' +
+                ", artist_country='" + artist_country + '\'' +
+                ", artist_rating=" + artist_rating +
+                '}';
+    }
+
+    public int getArtist_id() {
+        return artist_id;
+    }
+
+    public void setArtist_id(int artist_id) {
+        this.artist_id = artist_id;
+    }
+
+    public String getArtist_name() {
+        return artist_name;
+    }
+
+    public void setArtist_name(String artist_name) {
+        this.artist_name = artist_name;
+    }
+
+    public String getArtist_country() {
+        return artist_country;
+    }
+
+    public void setArtist_country(String artist_country) {
+        this.artist_country = artist_country;
+    }
+
+    public int getArtist_rating() {
+        return artist_rating;
+    }
+
+    public void setArtist_rating(int artist_rating) {
+        this.artist_rating = artist_rating;
+    }
 }
+
